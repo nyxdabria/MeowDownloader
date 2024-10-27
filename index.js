6420f2de3f4b80e3b6a9fd081bff0189af489bd2
Nyx Dabria <63550914+nyxdabria@users.noreply.github.com>
2024-10-27 23:49:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { execFile } = require("child_process");
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
